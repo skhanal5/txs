bc390dc53b18c20f87f13e467025a11be3ea7bd0
Subodh Khanal <skhanal5@terpmail.umd.edu>
2025-07-05 20:34:32 -0400

    Define health check handler. Update main function to spin up an http server with the handler registered to the /health endpoint. Add http folder for a sample health check request. Update gitignore to ignore the binary

--- cmd/txs/main.go
+++ cmd/txs/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/skhanal5/txs/internal/handler"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /health", handler.GetHealth)
+	log.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
