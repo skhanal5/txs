ed1542a888c07c13e1846df9eae8aa6362a280fa
Subodh Khanal <skhanal5@terpmail.umd.edu>
2025-07-07 19:40:38 -0400

    Rename user to auth service. Add implementation in repository layer to insert a user and to retrieve a user by their email address

--- internal/database/model/user.go
+++ internal/database/model/user.go
@@ -1,10 +1,6 @@
 package model
 
 type User struct {
-	ID        string `json:"id"`
-	Username  string `json:"username"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	Email     string `db:"email"`
+	Password  string `db:"password"`
 }
